44793e10ea520006c6ab930eec88dc612556f27d
sampsonsimpson <NNK@Neilss-MBP.home>
2016-06-24 14:56:21 -0400

    Course Project Submission github/sampsonsimpson
    Coursera | Getting and Cleaning Data | Course Project

---
+++ Run_Analysis.R
@@ -0,0 +1,81 @@
+#  Getting and Cleaning Data - Course Project
+#_____________________________________________
+
+#clear workspace memory
+rm(list=ls())
+
+#load dplyr and tidyr packages
+library(dplyr)
+library(tidyr)
+
+#Download and Unzip project files
+url <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+download.file(url, destfile = 'samsung.zip', method='curl')
+unzip('samsung.zip')
+
+#load features table
+features <- read.table('./UCI HAR Dataset/features.txt')
+#create logical vector for features with 'Mean' and 'Std' 
+featuresLite <- grep('-[Mm]ean()|-[Ss]td()', features[,2])
+#get just the feature names for 'Mean' and 'Std' filtering on logic above
+featureNames <- as.character(features[featuresLite,2])
+
+#clean up feature names and make them descriptive
+featureNames <- gsub('*-meanFreq\\()*', ' MeanFreq ', featureNames)
+featureNames <- gsub('*-mean\\()*', ' Mean ', featureNames)
+featureNames <- gsub('*-std\\()*', ' StdDev ', featureNames)
+
+#read in activity labels as new table
+activityLabels <- read.table('./UCI HAR Dataset/activity_labels.txt')
+colnames(activityLabels) <- c('activityID','activity')
+
+#Read in the Training set data but get only the 'Mean' and 'Std' data
+#  according to our featuresLite logical vector created above
+subjectTrain <- read.table('./UCI HAR Dataset/train/subject_train.txt')
+xtrain <- read.table('./UCI HAR Dataset/train/X_train.txt')[featuresLite]
+ytrain <- read.table('./UCI HAR Dataset/train/Y_train.txt')
+
+#Define column names for the training data
+colnames(subjectTrain) <- "subjectID"
+colnames(xtrain) <- featureNames
+colnames(ytrain) <- "activityID"
+
+#combine the subject, y, and x training sets into one
+trainSet <- cbind(subjectTrain,ytrain,xtrain)
+
+#Read in the Test set data but get only the 'Mean' and 'Std' data
+#  according to our featuresLite logical vector created above
+subjectTest <- read.table('./UCI HAR Dataset/test/subject_test.txt')
+xtest <- read.table('./UCI HAR Dataset/test/X_test.txt')[featuresLite]
+ytest <- read.table('./UCI HAR Dataset/test/Y_test.txt')
+
+colnames(subjectTest) <- "subjectID"
+colnames(xtest) <- featureNames
+colnames(ytest) <- "activityID"
+
+#combine the subject, y, and x test sets into one
+testSet <- cbind(subjectTest,ytest,xtest)
+
+#combine the training set and test set into one full set
+fullSet <- rbind(trainSet,testSet)
+
+#merge the fullSet dataset with the activityLabels table 
+fullSet <- merge(activityLabels,fullSet,'activityID')
+
+#gather columns into a long format table to create clean dataset
+cleanSet <- gather(fullSet,ncol=4:82)
+
+#create tidy set by
+# separating the feature column into the three delimited parts
+#    and labeling the new cols appropriately: 'feature','measure','XYZ'
+# grouping by all relevant columns
+# summarising by the group above for the average of all values
+# spreading the dataset by the newly created 'measure' column
+tidySet <- cleanSet %>%
+    separate(key, into=c('feature','measure','XYZ'), sep = ' ') %>%
+    group_by(feature, activityID, activity, subjectID, measure) %>%
+    summarise(Value = mean(value)) %>%
+    spread(measure, Value)
+    
+#print tidySet to console for viewing
+print(tbl_df(tidySet))
